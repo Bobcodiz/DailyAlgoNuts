e8dbaafe988d021dc228ad6d0f12849a293f878a
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-07-11 10:29:19 +0300

    DFS

--- Arrays/src/sorting/MergeSort.java
+++ Arrays/src/sorting/MergeSort.java
@@ -1,20 +1,29 @@
 package sorting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSort {
 
-    public List<Integer> mergeSort(int[] arr, int left, int right) {
+    public int[] divide(int[] arr, int left, int right) {
         left = 0;
         right = arr.length - 1;
         int mid = (left + right) / 2;
 
         if (left < right) {
             right = mid - 1;
+            divide(arr, left,right);
         }else {
             left = mid + 1;
+            divide(arr, left, right);
         }
-        return mergeSort(arr, left,right);
+        return arr;
+    }
+    public List<Integer> merge(int[] arr){
+        List<Integer> list = new ArrayList<>();
+
+        int i = 0, j = 0;
+        while()
     }
 
 }
